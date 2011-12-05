2929878ae89ce930cc5c7ffcbb5ace7d8c1c1ae4
Benjamin Reed <ranger@opennms.org>
2011-12-05 13:02:07 -0500

    make release configurable

--- makerpm.sh
+++ makerpm.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-mvn -Drpm=true "$@" clean install
+if [ -z "$OPENNMS_RELEASE" ]; then
+	OPENNMS_RELEASE=0.`date '+%Y%m%d'`.1
+fi
+mvn -Drpm=true -Drelease="$OPENNMS_RELEASE" clean install
 
 echo -e "Your RPM is: \c"
 find ncs-rpm -name \*.rpm
