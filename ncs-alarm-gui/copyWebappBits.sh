a9bd0f1a868e774b1fc1de785b773f0d135931eb
Donald <thedesloge@gmail.com>
2011-12-09 10:10:09 -0500

    add ncs-alarm-gui module

---
+++ ncs-alarm-gui/copyWebappBits.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp target/ncs-alarm-gui-1-SNAPSHOT/WEB-INF/jsp/alarm/ncs-alarms.jsp ~/git/opennms/target/opennms-1.9.94-SNAPSHOT/jetty-webapps/opennms/WEB-INF/jsp/alarm/ && cp target/ncs-alarm-gui-1-SNAPSHOT.jar ~/git/opennms/target/opennms-1.9.94-SNAPSHOT/jetty-webapps/opennms/WEB-INF/lib/
